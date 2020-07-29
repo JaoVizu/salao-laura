dd5b4c0673f470d8e2b438890c0ef678257995c2
Joao Vizu <joaovizu2015@gmail.com>
2020-07-29 12:04:10 -0300

    build

--- pages/pesquisa.js
+++ pages/pesquisa.js
@@ -7,6 +7,7 @@ const Pesquisa = () => {
 
   console.log(process.env.SHEET_CLIENT_EMAIL)
   console.log(process.env.SHEET_PRIVATE_KEY.replace(/\\n/g, "\n"))
+  console.log('oi')
 
   const [form, setForm] = useState({
     nome: '',
